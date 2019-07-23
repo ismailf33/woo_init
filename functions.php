7dd94b47cab3733cfcb8a4e44a7d81b47bc14f67
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-22 22:49:03 -0700

    @woocommerce support

--- functions.php
+++ functions.php
@@ -43,3 +43,8 @@ register_nav_menu('top_menu' , __('Top menu' , 'ecom'));
     }
 }
 add_action('init', 'ecomMenu');
+
+function ceom_woocommerce_support(){
+add_theme_support('woocommerce');
+}
+add_action('after_setup_theme' , 'ceom_woocommerce_support');
--- page.php
+++ page.php
@@ -1,18 +1,19 @@
 <?php get_header();?>
-<?php if(have_posts()) : while(have_posts()) : the_post() ?>
     <div class="body-content">
         <div class="container">
             <div class="row" style="margin-top: 30px;">
-                <div class="blog-page">
+                <div class="blog-page">                      
                     <div class="col-md-9">
-        <?php get_template_part('template-part/content/content','content')?>                
+                    <?php if(have_posts()) : while(have_posts()) : the_post() ?>
+                        <?php get_template_part('template-part/content/content','content')?>                
+                    <?php endwhile ;?>
+                    <?php endif ;?>
                     </div>
         <?php get_template_part('template-part/sidebar/right-sidebar','right-sidebar')?>
                 </div>
             </div>
         </div>
     </div>
-<?php endwhile ;?>
-<?php endif ;?>
+
 <?php get_footer();?>
 
