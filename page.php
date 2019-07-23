d3e55cf8420555cbf40451c9830063458dc51c92
alif33 <alifhasan332@gmail.com>
2019-07-23 09:50:50 +0600

    @initial setup

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php get_header();?>
+<?php if(have_posts()) : while(have_posts()) : the_post() ?>
+    <div class="body-content">
+        <div class="container">
+            <div class="row" style="margin-top: 30px;">
+                <div class="blog-page">
+                    <div class="col-md-9">
+        <?php get_template_part('template-part/content/content','content')?>                
+                    </div>
+        <?php get_template_part('template-part/sidebar/right-sidebar','right-sidebar')?>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endwhile ;?>
+<?php endif ;?>
+<?php get_footer();?>
