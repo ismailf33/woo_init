cc9e8fdd495673477d269e3260301444a8b1e8a3
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-24 14:08:41 -0700

    @remove default breadcam , result_counter

--- functions.php
+++ functions.php
@@ -70,3 +70,9 @@ function woocommerce_get_banner() {
   }
 add_filter('woocommerce_banner' ,'woocommerce_get_banner');
 
+if(!is_home()){
+  //remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0);
+  remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 ,0);
+  remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 ,0);
+  remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 ,0);
+  }
